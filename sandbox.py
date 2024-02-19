3b782c54257911e2525a40a066033534992815ab
ShubhamUpad <01shubhamupadhyay@gmail.com>
2024-02-20 02:24:56 +0530

    Ensuring that all keys are strings

--- sandbox.py
+++ sandbox.py
@@ -9,10 +9,13 @@ try:
         #print(type(data))
     if len(data) == 0 or (data[0] != "{" or data[-1] != "}"):
         print("Not a valid JSON file")
+    if data[-2] == ",":
+        print("Not a valid JSON file")
     else:
         print("Valid JSON File")
 except FileNotFoundError:
     print("File Not Found")
     
 print(data)
+print(type(data))
 
