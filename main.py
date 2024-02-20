43ed960396b9c48518dde45893b59b54678c21ee
ShubhamUpad <01shubhamupadhyay@gmail.com>
2024-02-20 13:15:50 +0530

    changed the structure of code

--- main.py
+++ main.py
@@ -1,35 +1,51 @@
 import sys
 import os.path
-errorCodes = {0:"Valid JSON File",
+
+class JsonParser:
+    def __init__(self,fileName):
+        
+        self.errorCodes = {0:"Valid JSON File",
               1:"Invalid JSON File",
               2:"File Not Found",
-              3:"Trailing comma after last key-value pair"}
-fileName = sys.argv[1]
-data = None
-isJson = True
-def checkCommaValid(string):
-    for char in range(len(string)):
-        if string[char] == "{" and string[char+1] != '"':
-            return [False,4]
-        if string[char] == ":" and string[char-1:char+2] != '":"':
-            #print('This condition ":"')
-            return [False,5]
-    return [True,0]
-try:
-    with open(r'tests\\tests\\step2\\'+fileName,"r") as file:
-        data = file.read()
-        #print(type(data))
-    if len(data) == 0 or (data[0] != "{" or data[-1] != "}"):
-        print(errorCodes[1] + " |     Error Code -" +"1")
-        isJson = not isJson
-    if data[-2] == ",":
-        print(errorCodes[3] + " |     Error Code -" +"1")
-        isJson = not isJson
-    if checkCommaValid(data)[0] is False:
-        print(checkCommaValid(data))
-    if isJson:
-        print("0")
-except FileNotFoundError:
-    print(errorCodes[2] + " |     Error Code -" + "2")
-if isJson:
-    print(data)
+              3:"Trailing comma after last key-value pair",
+              4:"Key not a string",
+              5: ""}
+        
+        self.fileName = fileName
+        self.data = None
+        self.isJson = True
+        self.checkCommaValidValue = []
+        
+    def checkCommaValid(self,string):
+        for char in range(len(string)):
+            if string[char] == "{" and string[char+1] not in ('"',"}"):
+                return [False,4]
+            if string[char] == ":" and string[char-1:char+2] != '":"':
+                #print('This condition ":"')
+                return [False,4]
+        return [True,0]
+    
+    def parseJSON(self):
+        try:
+            with open(r'tests\\tests\\step2\\'+self.fileName,"r") as file:
+                self.data = file.read()
+                #print(type(data))
+            if len(self.data) == 0 or (self.data[0] != "{" or self.data[-1] != "}"):
+                print(self.errorCodes[1] + " |     Error Code -" +"1")
+                self.isJson = not self.isJson
+            if len(self.data) > 2 and self.data[-2] == ",":
+                print(self.errorCodes[3] + " |     Error Code -" +"1")
+                self.isJson = not self.isJson
+            self.checkCommaValidValue = self.checkCommaValid(self.data)
+            if self.checkCommaValidValue[0] is False:
+                print(self.errorCodes[self.checkCommaValidValue[1]] + " |     Error Code -" + str(self.checkCommaValidValue[1]))
+                self.isJson = not self.isJson
+            if self.isJson:
+                print("0")
+        except FileNotFoundError:
+            print(self.errorCodes[2] + " |     Error Code -" + "2")
+        if self.isJson:
+            print(self.data)
+
+newParser = JsonParser(sys.argv[1])
+newParser.parseJSON()
--- sandbox.py
+++ sandbox.py
@@ -1,32 +1,46 @@
 import sys
 import os.path
 
-def checkCommaValid(string):
-    print(string)
-    for char in range(len(string)):
-        if string[char] == "{" and string[char+1] != '"':
-            return [False,4]
-        if string[char] == ":" and string[char-1:char+2] != '":"':
-            #print('This condition ":"')
-            return [False,5]
-    if string[-2] == ",":
-        return [False,6]
-    return [True,0]
-fileName = sys.argv[1]
-try:
-    with open(r'tests\\tests\\step2\\'+fileName,"r") as file:
-        data = file.read()
-        #print(type(data))
-    if len(data) == 0 or (data[0] != "{" or data[-1] != "}"):
-        print("Not a valid JSON file")
-    if data[-2] == ",":
-        print("Not a valid JSON file")
-    else:
-        print("Valid JSON File")
-    print(checkCommaValid(data))
-except FileNotFoundError:
-    print("File Not Found")
+class JsonParser:
+    def __init__(self,fileName):
+        
+        self.errorCodes = {0:"Valid JSON File",
+              1:"Invalid JSON File",
+              2:"File Not Found",
+              3:"Trailing comma after last key-value pair"}
+        
+        self.fileName = fileName
+        self.data = None
+        self.isJson = True
+        
+    def checkCommaValid(self,string):
+        for char in range(len(string)):
+            if string[char] == "{" and string[char+1] not in ('"',"}"):
+                return [False,4]
+            if string[char] == ":" and string[char-1:char+2] != '":"':
+                #print('This condition ":"')
+                return [False,5]
+        return [True,0]
     
-print(data)
-print(type(data))
+    def parseJSON(self):
+        try:
+            with open(r'tests\\tests\\step1\\'+self.fileName,"r") as file:
+                self.data = file.read()
+                #print(type(data))
+            if len(self.data) == 0 or (self.data[0] != "{" or self.data[-1] != "}"):
+                print(self.errorCodes[1] + " |     Error Code -" +"1")
+                self.isJson = not self.isJson
+            if len(self.data) > 2 and self.data[-2] == ",":
+                print(self.errorCodes[3] + " |     Error Code -" +"1")
+                self.isJson = not self.isJson
+            if self.checkCommaValid(self.data)[0] is False:
+                print(self.checkCommaValid(self.data))
+            if self.isJson:
+                print("0")
+        except FileNotFoundError:
+            print(self.errorCodes[2] + " |     Error Code -" + "2")
+        if self.isJson:
+            print(self.data)
 
+newParser = JsonParser(sys.argv[1])
+newParser.parseJSON()
