db2a1d9535e0512ad6ea2b3e071c95691ef538a6
ShubhamUpad <01shubhamupadhyay@gmail.com>
2024-02-21 15:14:54 +0530

    Removed extra spaces at the end

--- sandbox.py
+++ sandbox.py
@@ -9,10 +9,11 @@ class JsonParser:
               1:"JSON file is empty ",
               2:"File Not Found",
               3:"Trailing comma after last key-value pair",
-              4:"Trailing braces are absent",
+              4:"Preceeding and Trailing braces are absent",
               5:"Key is empty",
               6:"Key is not a string",
               7:"Value is not in proper format"}
+        
         self.successCodes = {
             101:"Basic checks passed",
             102:"Keys are in correct format",
@@ -26,12 +27,35 @@ class JsonParser:
         self.keyArr = []
         self.valueArr = []
     
+    def removeExtraSpaces(self,string):
+        counter = 0
+        for i in range(len(string)):    #remove spaces at the starting
+            if string[i] == " ":
+                counter += 1
+            else:
+                break
+        string = string[counter:]
+        
+        counter = len(string) - 1
+        for i in range(len(string)-1,-1,-1):
+            if string[i] == " ":
+                counter -= 1
+            else:
+                break
+        string = string[:counter]
+        
+        return string
+    
     def dataSplit(self,string): #do basic checks here for upto steps 
+        print(string)
         if len(string) == 0:    #if the JSON file is empty
             return [False,1]
+        string = self.removeExtraSpaces(string)
+        print(string)
         if len(string) == 2 and string != "{}": #self explainatory
             return [False,4]
         if len(string) > 2 and (string[0] != "{" or string[-1] != "}"): #ensure that curly baces are prest at either ends
+            print("Here")
             return [False,4]
         if len(string) > 2 and string[-2] == ",": #there is comma after last key value pair -> NOT VALID
             return [False,3]
@@ -57,6 +81,7 @@ class JsonParser:
                 isGood = True
             elif value in ('true','false','True','False'):     #boolean values allowed
                 isGood = True
+            #elif value
             if not isGood:
                 return [False,7]
         if isGood:
@@ -65,11 +90,12 @@ class JsonParser:
         
     def parseJSON(self):
         try:
-            with open(r'tests\\tests\\step1\\'+self.fileName,"r") as file:
+            with open(r'tests\\tests\\step2\\'+self.fileName,"r") as file:
                 self.data = file.read()
             self.basicCheck = self.dataSplit(self.data)
             if self.basicCheck[0] is False:
-                print("Error Code " + str(self.basicCheck[1]) + self.errorCodes[self.basicCheck[1]])
+                print("Failing Basic Checks")
+                print("Error Code " + str(self.basicCheck[1]) + " " + self.errorCodes[self.basicCheck[1]])
                 sys.exit()
             elif self.basicCheck[0] is True:    #basic checks satisfied
                 self.keyArr = self.basicCheck[1][0]
